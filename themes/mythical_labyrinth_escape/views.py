75e3d0656067fae2f154a0a3a07eeab092f9713b
Anastasia G <64243580+AnnaG07@users.noreply.github.com>
2020-09-21 12:39:15 -0400

    updated with num puzzle
    updated with num puzzle

--- themes/mythical_labyrinth_escape/views.py
+++ themes/mythical_labyrinth_escape/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, redirect, HttpResponse
 
 #all urls here are preappended with mythical_labyrinth_escape/
 def show_puzzle(request, puzzle_id): # url 'puzzle/<int:puzzle_id>'
-    puzzle_id = str(puzzle_id)
-    return HttpResponse(f"Show Puzzle {puzzle_id}")
+    #puzzle_id = str(puzzle_id)
+    #return HttpResponse(f"Show Puzzle {puzzle_id}")
+    context = {
+        'puzzle': Puzzles.objects.get(id=puzzle_id)
+    }
+    return render(request, f'puzzle{puzzle_id}', context)
 
 def answer(request, puzzle_id): # url 'puzzle/<int:puzzle_id>/answer'
     puzzle_id = str(puzzle_id)
