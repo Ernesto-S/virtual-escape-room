f8edf2aa1bbc2387fc0e56a726f008572e862f66
Anastasia G <64243580+AnnaG07@users.noreply.github.com>
2020-09-24 19:29:34 -0400

    push for merge conflict cleanup

--- themes/mythical_labyrinth_escape/urls.py
+++ themes/mythical_labyrinth_escape/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('puzzle/<int:puzzle_id>', views.show_puzzle),
     path('puzzle/<int:puzzle_id>/answer', views.answer),
     path('success_puzzle_1/<int:puzzle_id>', views.success_puzzle_1),
-    path('results/<int:puzzle_id>', views.show_results),
+    path('results', views.show_results),
 ]
--- themes/mythical_labyrinth_escape/views.py
+++ themes/mythical_labyrinth_escape/views.py
@@ -3,16 +3,12 @@ from virtual_escape_room_app.models import *
 
 #all urls here are preappended with mythical_labyrinth_escape/
 def show_puzzle(request, puzzle_id): # url 'puzzle/<int:puzzle_id>'
-    #puzzle_id = str(puzzle_id)
-    #return HttpResponse(f"Show Puzzle {puzzle_id}")
     context = {
         'puzzle': Puzzles.objects.get(id=puzzle_id)
     }
     return render(request, 'puzzle1.html', context)
 
 def answer(request, puzzle_id): # url 'puzzle/<int:puzzle_id>/answer'
-    #puzzle_id = str(puzzle_id)
-    #return HttpResponse(f"Check answer{puzzle_id}")
     answer = Puzzles.objects.get(answer=request.POST['answer'])
     return render(request, 'success_puzzle1.html')
 
@@ -20,5 +16,5 @@ def success_puzzle_1(request, id):
     puzzle = Puzzles.objects.get(id=3)
     return render(request, 'puzzle3.html')
 
-def show_results(request, puzzle_id): # url 'results'
+def show_results(request): # url 'results'
     return HttpResponse("Show results")
