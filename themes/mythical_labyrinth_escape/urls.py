166351861113b038cdc6629358c7c8dd371e81d3
Anastasia G <64243580+AnnaG07@users.noreply.github.com>
2020-09-25 22:35:49 -0400

    updated puzzle 3 to show image, ensured proper routing of puzzle and answer.

--- themes/mythical_labyrinth_escape/urls.py
+++ themes/mythical_labyrinth_escape/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('puzzle/2', views.show_puzzle_2),
     #path('success_puzzle_1', views.success_puzzle_1),
     path('puzzle/3', views.show_puzzle_3),
-    path('puzzle/2/answer', views.answer_2),
-    path('puzzle/3/answer', views.answer_3),
+    path('puzzle/2/answer_2', views.answer_2),
+    path('puzzle/3/answer_3', views.answer_3),
     path('results', views.show_results),
 ]
